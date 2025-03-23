517d5c87604421bca6bfa40af9415a91f4a785b8
Avinash Prajapati <aviavi61106@gmail.com>
2025-03-23 16:28:31 +0530

    Using the Custom permission class on the snippet views

--- tutorial_4/snippets/views.py
+++ tutorial_4/snippets/views.py
@@ -3,12 +3,14 @@ from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
 
 from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
+
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -19,7 +21,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
 
 
